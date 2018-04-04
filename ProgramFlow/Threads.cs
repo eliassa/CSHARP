1e3ce313c118f5d3155cef35d07384eda2f344d3
eliassa <esundbya@gmail.com>
2018-04-04 13:34:27 +0200

    First

---
+++ ProgramFlow/Threads.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProgramFlow
+{
+    public static class Threads
+    {
+        public static void RunThreads(int numOfThreads)
+        {
+            for(int i = 0; i < numOfThreads; i++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod))
+                {
+                    IsBackground = i % 2 == 1,
+                    Name = "T" + i,
+                    Priority = i == 2 ? ThreadPriority.Highest : ThreadPriority.Normal
+                };
+                Console.WriteLine("Starting thread " + t.Name);
+                t.Start(t);
+            }
+        }
+
+        private static void ThreadMethod(Object o)
+        {
+            Thread t = o as Thread;
+            Console.WriteLine("Thread name: " + t.Name + ", IsAlive: " + t.IsAlive + ", IsBackground: " + t.IsBackground + ", Priority: " + t.Priority.ToString());
+        }
+    }
+}
---
+++ Startup/Program.cs
@@ -0,0 +1,24 @@
+﻿using ProgramFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Startup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            Console.WriteLine(" ");
+
+
+            Threads.RunThreads(4);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Done!");
+        }
+    }
+}
